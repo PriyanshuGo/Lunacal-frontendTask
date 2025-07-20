c112d4df54bf314fdca6e69488d47ae76212c5e3
PriyanshuGo <priyanshusinghrawat84@gmail.com>
2025-07-20 21:33:19 +0530

    import aliase is working .

---
+++ src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Home: React.FC = () => {
+  return (
+    <div className="p-8">
+      <h1 className="text-3xl font-bold text-blue-600">Welcome to the Home Page!</h1>
+      <p className="mt-4 text-gray-700">
+        This is a simple Home component. You can customize this page with more content.
+      </p>
+    </div>
+  );
+};
+
+export default Home;
